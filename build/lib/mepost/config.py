4ddb6f8ad4c87157a1c578117904797609c4ce93
ertugrul kutluer <ertgrlkutluer@gmail.com>
2024-08-30 22:52:23 +0300

    update readme v1.0.4

--- build/lib/mepost/config.py
+++
@@ -1,4 +0,0 @@
-class MepostConfig:
-    def __init__(self, api_key: str):
-        self.api_key = api_key
-        self.base_url = "https://api.mepost.io/v1"
--- build/lib/mepost/exceptions.py
+++
@@ -1,7 +0,0 @@
-class MepostException(Exception):
-    pass
-
-
-class ApiException(MepostException):
-    def __init__(self, message: str):
-        super().__init__(message)
